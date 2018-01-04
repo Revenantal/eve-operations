6a625a54773955c2917bf9a83cb185d690a1b660
crazyjoery <dj-joer@hotmail.com>
2018-01-04 18:25:29 +0100

    Added flatpicker JS addon for easier date/time selection

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"  href="{{ mix('/css/flatpickr.css') }}">
 </head>
 <body>
     <div id="app">
