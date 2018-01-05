03a9ff0ac4f151df6d9ed2b39b0eceff363f5530
Revenantal <adrian2433@gmail.com>
2018-01-04 21:25:18 -0500

    Cleaning operations layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    @yield('styles');
+    @yield('styles')
 </head>
 <body>
     <div id="app">
--- resources/views/operations/create.blade.php
+++ resources/views/operations/create.blade.php
@@ -17,29 +17,39 @@
     <div class="container">
         <div class="well">
             <h2>Basic Operation Details</h2>
-                <div class="form-group">
-                    {{Form::label('name', 'Operation Name')}}<span class="text-danger">*</span>
-                    {{Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Imicus Doctrine'])}}
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        {{Form::label('name', 'Operation Name')}}<span class="text-danger">*</span>
+                        {{Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Imicus Doctrine'])}}
+                    </div>
                 </div>
-                <div class="form-group">
-                    {{Form::label('assigned_to', 'Assigned To')}}
-                    {{Form::text('assigned_to', '', ['class' => 'form-control', 'placeholder' => '1336468925'])}}
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        {{Form::label('assigned_to', 'Assigned To')}}
+                        {{Form::text('assigned_to', '', ['class' => 'form-control', 'placeholder' => 'Username'])}}
+                    </div>
                 </div>
-                <div class="form-group">
-                    {{Form::label('operation_type', 'Operation Type')}}<span class="text-danger">*</span>
-                    {{Form::select('operation_type', [
-                                        'structure_off' => 'Structure Offensive', 
-                                        'structure_def' => 'Structure Defensive',
-                                        'roam'          => 'Roam',
-                                        'general'       => 'General Fleet',
-                                        'fun'           => 'Fun Fleet',
-                                        'moon_mining'   => 'Moon Mining'],
-                                        null, ['class' => 'form-control', 'placeholder' => 'Select Type'])}}
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        {{Form::label('operation_type', 'Operation Type')}}<span class="text-danger">*</span>
+                        {{Form::select('operation_type', [
+                                            'structure_off' => 'Structure Offensive', 
+                                            'structure_def' => 'Structure Defensive',
+                                            'roam'          => 'Roam',
+                                            'general'       => 'General Fleet',
+                                            'fun'           => 'Fun Fleet',
+                                            'moon_mining'   => 'Moon Mining'],
+                                            null, ['class' => 'form-control', 'placeholder' => 'Select Type'])}}
+                    </div>
                 </div>
-                <div class="form-group">
-                    {{Form::label('operation_at', 'Operation Form Up')}}<span class="text-danger">*</span> <small>When the fleet should form (Eve Time)</small>
-                    {{Form::text('operation_at', '', ['class' => 'flatpickr form-control', 'placeholder' => 'Select Date...', 'data-id' => 'datetime'])}}
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        {{Form::label('operation_at', 'Operation Form Up')}}<span class="text-danger">*</span> <small>When the fleet should form (Eve Time)</small>
+                        {{Form::text('operation_at', '', ['class' => 'flatpickr form-control', 'placeholder' => 'Select Eve Date and Time', 'data-id' => 'datetime'])}}
+                    </div>
                 </div>
+            </div>
         </div>
     </div>
 
